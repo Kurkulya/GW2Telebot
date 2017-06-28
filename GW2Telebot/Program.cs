f38d907e10d22a7aa74e79db2c897cef91efd402
Kurkulya <kulinich_v@hotmail.com>
2017-06-28 19:01:33 +0300

    Added start bacgroundworker

--- GW2Telebot/Program.cs
+++ GW2Telebot/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace GW2Telebot
 {
     class Program
     {
+        BackgroundWorker bw;
+        public void Init()
+        {
+            this.bw = new BackgroundWorker();
+            this.bw.DoWork += this.bw_DoWork;
+        }
+
+        void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var worker = sender as BackgroundWorker;
+        }
+
         static void Main(string[] args)
         {
+            
         }
     }
 }
